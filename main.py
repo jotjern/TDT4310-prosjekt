93bdb14e34b1de04051e326533de77ec9ca69f3c
jogt <jotjernshaugen@gmail.com>
2023-04-21 13:27:50 +0200

    Cleaned up test code

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+from typing import Callable
+
+import pandas as pd
+
 import dataset_downloader
 from pyarrow import feather
 from tqdm import tqdm
 from collections import Counter
 from sentiment_analysis import sentiment_analysis
 
-if __name__ == "__main__":
+
+def f1_evaluate_model(test_df: pd.DataFrame, predict: Callable[[str], int], model_name: str = "") -> float:
+    correct = 0
+    total = 0
+    for _, row in tqdm(test_df.iterrows(), total=len(test_df), desc=f"Evaluating model {model_name}"):
+        prediction = predict(row["title"])
+        if prediction == row["value"]:
+            correct += 1
+        total += 1
+
+    return correct / total
+
+
+def main():
     dataset_downloader.download_kaggle_dataset()
     dataset_downloader.download_yahoo_stock_data()
 
@@ -33,23 +50,21 @@ if __name__ == "__main__":
         if total > 0:
             word_weights[word] = positive / total
 
-    n_correct = 0
-    for _, row in tqdm(test_df.iterrows(), desc="Testing bag of words model"):
-        predicted_value = sum(
-            word_weights.get(word) for word in row["title"].lower().split() if word in word_weights) / len(
-            row["title"].split()
+    def predict_bag_of_words(title: str) -> int:
+        return sum(
+            word_weights.get(word) for word in title.lower().split() if word in word_weights) / len(
+            title.split()
         ) > 0.5
 
-        if predicted_value == row["value"]:
-            n_correct += 1
+    def predict_sentiment_analysis(title: str) -> int:
+        return sentiment_analysis(title) > 0
 
-    print(f"Bag of words F1 score: {n_correct / len(test_df)}")
+    bag_of_words_f1 = f1_evaluate_model(test_df, predict_bag_of_words, "Bag of words")
+    sentiment_analysis_f1 = f1_evaluate_model(test_df, predict_sentiment_analysis, "Sentiment analysis")
 
-    n_correct = 0
-    for _, row in tqdm(test_df.iterrows(), desc="Testing sentiment analysis model"):
-        predicted_value = sentiment_analysis(row["title"]) > 0
+    print("Bag of words F1:", bag_of_words_f1)
+    print("Sentiment analysis F1:", sentiment_analysis_f1)
 
-        if predicted_value == row["value"]:
-            n_correct += 1
 
-    print(f"Sentiment analysis F1 score: {n_correct / len(test_df)}")
+if __name__ == "__main__":
+    main()
