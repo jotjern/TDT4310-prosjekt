87738094de0d139b5c471568ce0828fc33d9af3b
Knut Einar Gilja Johansen <kegjohan@stud.ntnu.no>
2023-04-24 10:27:33 +0100

    knudi

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from models import BagOfWordsModel, SentimentAnalysisModel, PredictionModel, Log
 def main():
     print("Loading data...")
     train_df, test_df = dataset_downloader.load_data()
+    print(train_df)
 
     models = [LogisticRegression()]
 
--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,4 +1,7 @@
+from tqdm import tqdm
 from .prediction_model import PredictionModel
+from .sentiment_analysis import sentiment_analysis
+import re
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression as LR
@@ -11,13 +14,30 @@ class LogisticRegression(PredictionModel):
         self.count_vectorizer = CountVectorizer(stop_words="english")
         self.model = LR()
         
+    def preprocess(self, text):
+        text = text.lower()
+        text = re.sub(r'[^\w\s]','', text)
+        text = re.sub(r'\d+', '', text)
+        
+        return text
+        
     def train(self, train_df):
-        x = self.count_vectorizer.fit_transform(train_df['title'])
         y = train_df['value']
         
-        self.model.fit(x, y)
+        titles = []
+        sentiment = []
+        for _, row in tqdm(train_df.iterrows(), total=len(train_df), desc=f"Training {self.name} model"):
+            i = row['title']
+            titles.append(self.preprocess(i))
+            sentiment.append(sentiment_analysis(i))
         
-        print("Accuracy: ", self.model.score(x, y))
+        X_titles = self.count_vectorizer.fit_transform(titles)
+        X = list(zip(X_titles.toarray(), [s for _, s in sentiment]))
+        print(X)
+        
+        self.model.fit(X, y)
+        
+        print("Accuracy: ", self.model.score(X, y))
         
     def predict(self, title: str) -> int:
         title_vec = self.count_vectorizer.transform([title])
